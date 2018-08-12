b5118136a067af45d7650444ad6593d649df4bcb
Jake Barter <MidgetJake@users.noreply.github.com>
2018-08-12 01:44:04 +0100

    More generation & sector safety stuff.

--- Assets/Scripts/Environment/Sector.cs
+++ Assets/Scripts/Environment/Sector.cs
@@ -1,11 +1,23 @@
-﻿using Game;
+﻿using System.Collections;
+using Game;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Environment {
+
+	public struct SectorData {
+		public Sector attachedSector;
+		public string attachedSide;
+		public bool isSealed;
+	}
+	
 	public class Sector : MonoBehaviour {
 		[SerializeField] private MainController m_GameController;
+		[SerializeField] private GameObject m_GasArea;
 		
-		public float m_DestructionTime;
+		public List<SectorData> attachedSectors = new List<SectorData>();
+		public bool isSafe = true;
+		public float destructionTime;
 		
 		public bool DIEDIEDIE = false;
 		public bool goingToDie = false;
@@ -17,22 +29,38 @@ namespace Environment {
 			}
 			
 			if (goingToDie) {
-				m_DestructionTime -= Time.deltaTime;
+				destructionTime -= Time.deltaTime;
 
-				if (m_DestructionTime <= 1.5f) {
+				/*if (destructionTime <= 1.5f) {
 					m_GameController.Play_Destruction(gameObject);
-				}
+				}*/
 				
-				if (m_DestructionTime <= 0) {
-					Destroy(gameObject);
+				if (destructionTime <= 0) {
+					StartCoroutine(DestroySector());
 				}
 			}
 		}
 
 		public void InitiateCountDown() {
 			goingToDie = true;
-			m_DestructionTime = Random.Range(15, 15);
-			m_GameController.Play_DestructionWarning();
+			destructionTime = Random.Range(15, 15);
+//			m_GameController.Play_DestructionWarning();
+		}
+
+		public IEnumerator DestroySector() {
+			isSafe = false;
+			yield return new WaitForSeconds(5f);
+			foreach (SectorData sect in attachedSectors) {
+				if (!sect.isSealed && sect.attachedSector.isSafe) {
+					StartCoroutine(sect.attachedSector.DestroySector());
+				}
+			}
+		}
+
+		public void AddSection(SectorData newSector) {
+			print("Got another!");
+			print(newSector);
+			attachedSectors.Add(newSector);
 		}
 	}
 }
--- Assets/Scripts/Environment/SectorGenerator.cs
+++ Assets/Scripts/Environment/SectorGenerator.cs
@@ -15,6 +15,7 @@ namespace Environment {
 		private Dictionary<string, float> m_RotateValues = new Dictionary<string, float>();
 		private bool m_Setup;
 		private int m_CreationAttempts = 0;
+		private bool m_HasCast;
 		
 		public void Generate() {
 			if (!m_Setup) {
@@ -39,21 +40,90 @@ namespace Environment {
 			m_CreationAttempts++;
 			bool valid = true;
 			Vector3 newPosition = transform.position + ((transform.position - child.position) * 2);
-			foreach (Vector3 point in EnvironmentSettings.sectorPoints) {
+//			Vector3 newPosition = transform.position + ((transform.position - child.position) * 1.1f);
+			/*foreach (Vector3 point in EnvironmentSettings.sectorPoints) {
 				if (
 					point.x - newPosition.x <= 0.01f && point.x - newPosition.x >= -0.01f &&
 					point.y - newPosition.y <= 0.01f && point.y - newPosition.y >= -0.01f &&
 					point.z - newPosition.z <= 0.01f && point.z - newPosition.z >= -0.01f
-				) {
-					valid = false;
-					if (m_CreationAttempts < 5) {
-						GenerateSector(child);
+				) {*/
+
+			//if (!m_HasCast) {
+				Vector3 top = new Vector3(newPosition.x , newPosition.y + 0.5f, newPosition.z);
+				RaycastHit hit;
+				if (Physics.Raycast(top, Vector3.down, out hit)) {
+					if (hit.collider.transform.position != transform.position) {
+						Vector3 testPosition = transform.position + ((transform.position - child.position) * 1.1f);
+						testPosition.y += 0.5f;
+						Debug.DrawLine(testPosition, testPosition + Vector3.down, Color.red, 100f);
+						if (Physics.Raycast(testPosition, Vector3.down, out hit)) {
+							valid = false;
+							print("One here already!");
+							SectorData sectorData = new SectorData();
+							sectorData.attachedSector = hit.collider.GetComponent<Sector>();
+							sectorData.isSealed = false;
+							sectorData.attachedSide = child.name;
+							transform.GetComponent<Sector>().attachedSectors.Add(sectorData);
+
+							SectorData parentSectorData = new SectorData();
+							parentSectorData.attachedSector = transform.GetComponent<Sector>();
+							parentSectorData.isSealed = false;
+							parentSectorData.attachedSide = "Unknown";
+							hit.collider.GetComponent<Sector>().AddSection(parentSectorData);
+							m_HasCast = true;
+						}
+
+						/*if (m_CreationAttempts < 5) {
+							GenerateSector(child);
+						}*/
+					}
+				} else {
+					m_HasCast = false;
+					SectorGenerator nextSector = Instantiate(m_SectorList[Random.Range(0, m_SectorList.Count - 1)]);
+					
+					nextSector.attachedIndex = Random.Range(0, nextSector.points.Count - 1);
+					Transform attachedPoint = nextSector.points[nextSector.attachedIndex];
+					float baseRotate = m_RotateValues[child.name] - m_RotateValues[attachedPoint.name] + selfRotate;
+					nextSector.transform.rotation = Quaternion.Euler(-90, 0, baseRotate);
+					nextSector.selfRotate = baseRotate;
+					
+					//nextSector.transform.SetParent(transform);
+					nextSector.transform.position = newPosition;
+					nextSector.transform.position = new Vector3(nextSector.transform.position.x, 0, nextSector.transform.position.z);
+
+					m_CreationAttempts = 0;
+					//nextSector.transform.SetParent(EnvironmentSettings.topParent);
+					EnvironmentSettings.sectorPoints.Add(nextSector.transform.position);
+				
+					//nextSector.transform.SetParent(null);
+				
+					SectorData parentSectorData = new SectorData();
+					parentSectorData.attachedSector = transform.GetComponent<Sector>();
+					parentSectorData.isSealed = false;
+					parentSectorData.attachedSide = attachedPoint.name;
+					nextSector.GetComponent<Sector>().attachedSectors.Add(parentSectorData);
+				
+					SectorData sectorData = new SectorData();
+					sectorData.attachedSector = nextSector.GetComponent<Sector>();
+					sectorData.isSealed = false;
+					sectorData.attachedSide = child.name;
+					transform.GetComponent<Sector>().attachedSectors.Add(sectorData);
+				
+					EnvironmentSettings.sectorList.Add(nextSector.GetComponent<Sector>());
+				
+					if (EnvironmentSettings.passCount < EnvironmentSettings.generationPasses) {
+						nextSector.Generate();
 					}
 				}
-			}
+			//}
+
+					
+				//}
+			//}
 			
 			
-			if(valid) {
+			if (valid) {
+				/*m_HasCast = false;
 				SectorGenerator nextSector = Instantiate(m_SectorList[Random.Range(0, m_SectorList.Count - 1)]);
 					
 				nextSector.attachedIndex = Random.Range(0, nextSector.points.Count - 1);
@@ -67,18 +137,28 @@ namespace Environment {
 				nextSector.transform.position = new Vector3(nextSector.transform.position.x, 0, nextSector.transform.position.z);
 
 				m_CreationAttempts = 0;
-				nextSector.transform.SetParent(EnvironmentSettings.topParent);
+				//nextSector.transform.SetParent(EnvironmentSettings.topParent);
 				EnvironmentSettings.sectorPoints.Add(nextSector.transform.position);
-				print(" - - - - ");
-				print(transform.position + ((transform.position - child.position) * 2));
-				print(" - - - - ");
 				
 				//nextSector.transform.SetParent(null);
 				
+				SectorData parentSectorData = new SectorData();
+				parentSectorData.attachedSector = transform.GetComponent<Sector>();
+				parentSectorData.isSealed = false;
+				parentSectorData.attachedSide = attachedPoint.name;
+				nextSector.GetComponent<Sector>().attachedSectors.Add(parentSectorData);
+				
+				SectorData sectorData = new SectorData();
+				sectorData.attachedSector = nextSector.GetComponent<Sector>();
+				sectorData.isSealed = false;
+				sectorData.attachedSide = child.name;
+				transform.GetComponent<Sector>().attachedSectors.Add(sectorData);
+				
 				EnvironmentSettings.sectorList.Add(nextSector.GetComponent<Sector>());
+				
 				if (EnvironmentSettings.passCount < EnvironmentSettings.generationPasses) {
 					nextSector.Generate();
-				}
+				}*/
 			}
 		}
 	}
